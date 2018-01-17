aff85fde707d4e115f15c9a8a52acbf586fb5caa
aldb <alexs.dbk@gmail.com>
2018-01-16 20:41:48 -0800

    Added selectionsort

---
+++ selectionsort.js
@@ -0,0 +1,27 @@
+function selectionSort(items){
+    for (i=0; i < items.length; i++) {
+        var min = i;
+
+        for (k=i+1; k < items.length; k++) {
+            if (items[k] < items[min]){
+                min = k;
+            }
+        }
+
+        if (i !== min) {
+            (function swap(items, firstIndex, secondIndex){
+                var temp = items[firstIndex];
+                items[firstIndex] = items[secondIndex];
+                items[secondIndex] = temp;
+            })(items, i, min);
+        }
+    }
+    return items;
+}
+
+
+[
+    '123216443658743695943758432675983768943',
+    '000000000111111100000000000000000011111',
+    'dfasfjasdfghjsadgfagjshd'
+].map(str => console.log(selectionSort(str.split(''))) );
