36a37d29caad07c97b87a6dd9075079d7b384192
aldb <alexs.dbk@gmail.com>
2018-05-04 18:42:53 -0700

    Added hash open addresing

---
+++ hash_open_addressing.js
@@ -0,0 +1,75 @@
+function HashTable() {
+  var vm = this;
+  vm._bucketSize = 10;
+  vm._buckets = [];
+  vm._buckets.length = vm._bucketSize;
+
+  vm.hashCode = function(str) {
+    var hash = 0, i, chr;
+    if (str.length === 0) return hash;
+    for (i = 0; i < str.length; i++) {
+    chr   = str.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+    }
+    return hash % vm._bucketSize
+  };
+
+  vm.get = function(key) {
+    var hash = vm.hashCode(key);
+      if (!vm._buckets[hash]) {
+      return undefined;
+    }
+    if (vm._buckets[hash].hasOwnProperty(key)) {
+      return vm._buckets[hash][key];
+    }
+    var bucketId = hash + 1;
+    while (bucketId != hash) {
+      if (bucketId >= vm._bucketSize) {
+        bucketId = 0;
+      }
+
+      if (vm._buckets[bucketId] === undefined) {
+        return undefined;
+      }
+      if (vm._buckets[bucketId].hasOwnProperty(key)) {
+        return vm._buckets[hash][key];
+      }
+      bucketId++;
+    };
+    return undefined;
+  };
+
+  vm.put = function(key, value) {
+    var hash = vm.hashCode(key);
+    if (!vm._buckets[hash]) {
+      vm._buckets[hash] = {};
+      vm._buckets[hash][key] = value;
+      return;
+    }
+    if (vm._buckets[hash].hasOwnProperty(key)) {
+      throw 'Duplicate not allowed';
+    }
+
+    var bucketId = hash + 1;
+    while (bucketId !== hash) {
+      if (bucketId >= vm._bucketSize) {
+        bucketId = 0;
+      }
+
+      if (!vm._buckets[bucketId]) {
+        vm._buckets[bucketId] = {};
+        vm._buckets[bucketId][key] = value;
+        return;
+      }
+
+      bucketId++;
+    }
+    throw 'HashTable is full, a rehash is needed';
+  };
+}
+var hashTableObj = new HashTable();
+for (var i = 0; i < 100; i++) {
+hashTableObj.put(`element${i}`, i);
+}
+console.log(hashTableObj);
