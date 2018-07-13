2aeeac28a64bbcdd920ba6fff12bc726e78374a8
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-07-13 04:43:09 -0700

    Update depth_first_search_recursion.js

--- depth_first_search_recursion.js
+++ depth_first_search_recursion.js
@@ -1,30 +1,36 @@
 /*
- * The recursive implementation breadth-first search traversal
+ * The recursive implementation breadth-first search
  * Performs a recursion depth-first search on a graph represent an object of array edges
  * @param {string} source - The source vertex.
  * @param {object} tree - Graph, represented as object.
+ * @param {string} search - The search node
  * @returns {object} Object of parents: {'A' : 'None', 'B' : 'A', 'C' : 'A'}
  */
-function depth_first_search_traversal(root, tree) {
+function depth_first_search(root, tree, search) {
   let visited   = {};
   visited[root] = 'None';
 
-  recursionDFSvisit = function(source, tree) {
-	for (let i = 0; i < tree[source].length; i++) {
-	    const vertex = tree[source][i];
-            if (!visited[vertex]) {
-                visited[vertex] = source;
-                recursionDFSvisit(vertex, tree);
+  recursionDFSvisit = function(source, tree, search) {
+    if (source === search) {
+        return true;
+    }
+    for (let i = 0; i < tree[source].length; i++) {
+        const vertex = tree[source][i];
+        if (!visited[vertex]) {
+            visited[vertex] = source;
+            if (recursionDFSvisit(vertex, tree, search) === true) {
+                return true;
             }
-	}
+        }
+    }
+    return false;
   };
   // Search from start vertex root
-  recursionDFSvisit(root, tree);
-  return visited;
+  return recursionDFSvisit(root, tree, search);
 };
 
 (() => {
-  console.log('Testing started: depth-first search traversal');
+  console.log('Testing started: depth-first search');
   const it = ((description, func) => {
     console.log(' => ' + description + ' => ' +  func());
   });
@@ -49,31 +55,34 @@ function depth_first_search_traversal(root, tree) {
       'J' : [],
     };
 
-  it('#1 Test root node', () => {
+  it('#1 Search root node', () => {
+    const root     = 'A';
     const input    = 'A';
-    const expect   = 'None';
-    const response = depth_first_search_traversal(input, adjacencyMatrix);
-    if (expect === response[input]) {
+    const expect   = true;
+    const response = depth_first_search(root, adjacencyMatrix, input);
+    if (expect === response) {
       return 'PASSED';
     }
     return 'FAILED; EXPECT:' + expect + ' !== ' + response;
   });
 
-  it('#2 Test parent of leaf J', () => {
-    const input    = 'A';
-    const expect   = 'F';
-    const response = depth_first_search_traversal(input, adjacencyMatrix);
-    if (expect === response['J']) {
+  it('#1 Search not exist node', () => {
+    const root     = 'A';
+    const input    = 'X';
+    const expect   = false;
+    const response = depth_first_search(root, adjacencyMatrix, input);
+    if (expect === response) {
       return 'PASSED';
     }
     return 'FAILED; EXPECT:' + expect + ' !== ' + response;
   });
 
-  it('#3 Test parent of leaf G', () => {
-    const input    = 'A';
-    const expect   = 'D';
-    const response = depth_first_search_traversal(input, adjacencyMatrix);
-    if (expect === response['G']) {
+  it('#1 Search leaf node', () => {
+    const root     = 'A';
+    const input    = 'G';
+    const expect   = true;
+    const response = depth_first_search(root, adjacencyMatrix, input);
+    if (expect === response) {
       return 'PASSED';
     }
     return 'FAILED; EXPECT:' + expect + ' !== ' + response;
