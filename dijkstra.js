bac231ac6b48fabfbd166372f4b71a59f1665f87
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-07-14 19:02:29 -0700

    Update dijkstra.js

--- dijkstra.js
+++ dijkstra.js
@@ -1,90 +1,86 @@
-function dijkstra(graph, start) {
-  var solutions = {};
-  solutions[start] = [];
-  solutions[start].dist = 0;
-  
-  while (true) {
-    var parent = null;
-    var nearest = null;
-    var dist = Infinity;
-
-    //for each existing solution
-    for (var n in solutions) {
-      if (!solutions[n]) {
-        continue
-      }
-      var ndist = solutions[n].dist;
-      var adj = graph[n];
-      //for each of its adjacent nodes...
-      for (var a in adj) {
-        //without a solution already...
-        if (solutions[a]) {
-          continue;
-        }
-
-        //choose nearest node with lowest *total* cost
-        var d = adj[a] + ndist;
-        if (d < dist) {
-          //reference parent
-          parent  = solutions[n];
-          nearest = a;
-          dist    = d;
-        }
-      }
+/**
+ * Dijkstra algorithm implementation based on simplify priority Queue (Array + splice)
+ * @param  {string} root
+ * @param  {object} graph - Adjacency list
+ * @return {object} - 'A' : {distance : 0, parent: 'None'}
+ */
+function dijkstra(root, graph) {
+    const priorityQueueInsertOrUpdate = function(current) {
+        for (var i = 0; i < priorityQueue.length; i++) {
+            if (distance[current].distance > distance[priorityQueue[i]].distance) break;
+        }                
+        priorityQueue.splice(i, 0, current);
     }
-    
-    //no more solutions
-    if (dist === Infinity) {
-        break;
+
+    const distance = {[root] : {distance : 0, parent: 'None'}};
+    const priorityQueue = [root];
+
+    while(priorityQueue.length) {
+        const current = priorityQueue.shift();
+        for (node in graph[current]) {
+            const weigth = graph[current][node];
+            if (!distance[node] || distance[node] > distance[current].distance + weigth) {
+                distance[node] = {distance : distance[current].distance + weigth, parent: current};
+                priorityQueueInsertOrUpdate(node);
+            }
+        }
     }
-    
-    //extend parent's solution path
-    solutions[nearest] = parent.concat(nearest);
-    //extend parent's cost
-    solutions[nearest].dist = dist;
-  }
-  
-  return solutions;
+    return distance;
 }
 
-var graph = {
-  "2":{"3":1,"4":1,"R":1},
-  "3":{"2":1,"4":1,"6":1,"13":1},
-  "4":{"2":1,"3":1,"5":1,"8":1},
-  "5":{"4":1,"7":1,"11":1},
-  "6":{"3":1,"13":1,"15":1},
-  "7":{"5":1,"10":1},
-  "8":{"4":1,"11":1,"13":1},
-  "9":{"14":1},"10":{"7":1},
-  "11":{"5":1,"8":1,"12":1},
-  "12":{"11":1},
-  "13":{"3":1,"6":1,"8":1,"14":1},
-  "14":{"9":1,"13":1},
-  "15":{"6":1},
-  "R":{"2":1}
-};
 
-/*
-Testing From 10
-10 -> 2: 7 -> 5 -> 4 -> 2 distance: 4
-10 -> 3: 7 -> 5 -> 4 -> 3 distance: 4
-10 -> 4: 7 -> 5 -> 4 distance: 3
-10 -> 5: 7 -> 5 distance: 2
-10 -> 6: 7 -> 5 -> 4 -> 3 -> 6 distance: 5
-10 -> 7: 7 distance: 1
-10 -> 8: 7 -> 5 -> 4 -> 8 distance: 4
-10 -> 9: 7 -> 5 -> 4 -> 3 -> 13 -> 14 -> 9 distance: 7
-10 -> 10:  distance: 0
-10 -> 11: 7 -> 5 -> 11 distance: 3
-10 -> 12: 7 -> 5 -> 11 -> 12 distance: 4
-10 -> 13: 7 -> 5 -> 4 -> 3 -> 13 distance: 5
-10 -> 14: 7 -> 5 -> 4 -> 3 -> 13 -> 14 distance: 6
-10 -> 15: 7 -> 5 -> 4 -> 3 -> 6 -> 15 distance: 6
-10 -> R: 7 -> 5 -> 4 -> 2 -> R distance: 5
-*/
+(() => {
+  console.log('Testing started: Dijkstra');
+  const it = ((description, func) => {
+    console.log(' => ' + description + ' => ' +  func());
+  });
 
-var solutions = dijkstra(graph, 10);
-for (var s in solutions) {
-  if(!solutions[s]) continue;
-  console.log(start + ' -> ' + solutions[s].join(" -> ") + "; distance: " + solutions[s].dist);
-}
+//              A
+//            /   \
+//          1      1
+//         /        \
+//        B          C
+//      /   \          \
+//    1       1        10
+//   /         \         \
+//  D          E -- 1 -- F
+    const adjacencyMatrix = {
+      'A' : {'B' : 1, 'C' : 1},
+      'B' : {'D' : 1, 'E' : 1},
+      'C' : {'F' : 10},
+      'D' : {},
+      'E' : {'F' : 1},
+      'F' : {},
+    };
+
+  it('#1 Test root node', () => {
+    const input    = 'A';
+    const expect   = 'None';
+    const response = dijkstra(input, adjacencyMatrix);
+    console.log(response)
+    if (expect === response[input].parent) {
+      return 'PASSED';
+    }
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+
+  it('#2 Test shortest path to F ', () => {
+    const input    = 'A';
+    const expect   = 3;
+    const response = dijkstra(input, adjacencyMatrix);
+    if (expect === response['F'].distance) {
+      return 'PASSED';
+    }
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+
+  it('#3 Test shortest path to not existing', () => {
+    const input    = 'A';
+    const expect   = 'D';
+    const response = dijkstra(input, adjacencyMatrix);
+    if (expect === response['G']) {
+      return 'PASSED';
+    }
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+})();
