d3491713cf5cd024f20a68fcef8c562488951b93
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-07-15 11:20:28 -0700

    Update breadth_first_search_objs_arr.js

--- breadth_first_search_objs_arr.js
+++ breadth_first_search_objs_arr.js
@@ -1,80 +1,85 @@
 /*
- * Performs a breadth-first search on a graph represent an object of array edges
- * @param {string} source - The source vertex.
+ * The non-recursive implementation an breadth-first search (queue - Array)
+ * Performs a breadth-first search on a graph represent an object of array edges(Adjacency matrix)
+ * @param {string} root - The root|source vertex.
  * @param {object} tree - Graph, represented as object.
- * @returns {object} Object of objects describing each vertex, like
- *     { vertex : {distance: _, parent: _ } }
+ * @returns {object|false} Object of distances and parents with shortes path: {A :{distance: 0, parent: "None"}} or false
  */
-function doBFS(sourse, tree) {
-    var pathInfo      = {};
-    var level         = 1;
-    var frontier      = [sourse];
-    pathInfo[sourse]  = {dinstance : 0, parent: null};
-    while(frontier.length) {
-        var next = [];
-        frontier.map(function(u) {
-            tree[u].map(function(v) {
-                if (pathInfo[v] === undefined) {
-                    pathInfo[v] = {dinstance : level, parent: u};
-                    next.push(v);
-                }
-            });
-        });
-        frontier = next;
-        level++;
+function breadth_first_search(root, graph, search) {
+    const distance = {[root] : {distance : 0, parent: 'None'}};
+    const queue = [root];
+    while(queue.length) {
+        const current = queue.shift();
+		if (current === search) {
+			return distance;
+		}
+        for (let i = 0; i < graph[current].length; i++) {
+            const node = graph[current][i];
+            if (!distance[node]) {
+                distance[node] = {distance : distance[current].distance + 1, parent: current};
+                queue.push(node);
+            }
+        }
     }
-    return pathInfo;
+    return false;
 };
 
-// Uncomment for testing
-/*
-var tree = {
-        'A': ['B', 'C', 'D', 'E'],
-        'B': ['A', 'F', 'G'],
-        'C': ['A', 'H'],
-        'D': ['A', 'I', 'J'],
-        'E': ['A', 'K', 'L'],
-        'F': ['B', 'G', 'M', 'N', 'O'],
-        'G': ['B', 'F', 'P', 'Q', 'R'],
-        'H': ['C', 'S'],
-        'I': ['D'],
-        'J': ['D', 'T', 'U'],
-        'K': ['E'],
-        'L': ['E', 'V'],
-        'M': ['F'],
-        'N': ['F'],
-        'O': ['F'],
-        'P': ['G'],
-        'Q': ['G'],
-        'R': ['G'],
-        'S': ['H', 'W', 'X'],
-        'T': ['J'],
-        'U': ['J', 'Y', 'Z'],
-        'V': ['L'],
-        'W': ['S'],
-        'X': ['S'],
-        'Y': ['U'],
-        'Z': ['U']
-};
-for (root in tree) {
-    var info = doBFS(root, tree);
-    console.log('Generated path info for ' + root + '.', info);
-    for (pathTo in info) {
-        printPathTo(pathTo, info, root)
-    }
-}
+(() => {
+  console.log('Testing started: breadth first search traversal');
+  const it = ((description, func) => {
+    console.log(' => ' + description + ' => ' +  func());
+  });
 
-function printPathTo(source, info, root) {
-    var path = [source];
-    path.push(info[source].parent);
-    var next = info[source].parent;
-    if (!info.hasOwnProperty(next)) {
-        return console.log('          ' + source + ' is root');
+    //      A
+    //    /  \
+    //   B    C
+    //  / \    \
+    // D   E   F 
+    /// \     / \
+    //G H    I   J
+    const adjacencyMatrix = {
+      'A' : ['B', 'C'],
+      'B' : ['D', 'E'],
+      'C' : ['F'],
+      'D' : ['G', 'H'],
+      'E' : [],
+      'F' : ['I', 'J'],
+      'G' : [],
+      'H' : [],
+      'I' : [],
+      'J' : [],
+    };
+
+  it('#1 Test root node', () => {
+    const input    = 'A';
+	const search   = 'A';
+    const expect   = {distance : 0, parent: 'None'};
+    const response = breadth_first_search(input, adjacencyMatrix, search);
+    if (expect.parent === response[input].parent) {
+      return 'PASSED';
     }
-    while(info[next].parent) {
-        path.push(info[next].parent);
-        next = info[next].parent;
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+
+  it('#2 Test parent of leaf J', () => {
+    const input    = 'A';
+	const search   = 'J';
+    const expect   = {distance: 3, parent: "F"};
+    const response = breadth_first_search(input, adjacencyMatrix, search);
+    if (expect.parent === response['J'].parent) {
+      return 'PASSED';
     }
-    console.log('          Distance to ' + source + ':' + info[source].dinstance + '; short path: ' + path.reverse().join(' -> ')); 
-}
-*/
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+
+  it('#3 Test parent of leaf M - not exist', () => {
+    const input    = 'A';
+	const search   = 'M';
+    const expect   = false;
+    const response = breadth_first_search(input, adjacencyMatrix, search);
+    if (expect === response) {
+      return 'PASSED';
+    }
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+})();
