315e58149b425c51b1deb5931a8146434d2dd9f6
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-07-17 08:14:49 -0700

    Update topological_sort.js

--- topological_sort.js
+++ topological_sort.js
@@ -3,18 +3,19 @@
  * vertex u comes before v in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
  *
  * A DAG G has at least one vertex with in-degree 0 and one vertex with out-degree 0
- *
+ * @param {object} graph
+ * @return {array} sorted Array || Exception Error
  */
 function topologicalSort(graph) {
-  var result  = [];
-  var visited = [];
-  var temp    = [];
+  const result  = [];
+  const visited = [];
+  const temp    = [];
 
-  var topologicalSortRecursion = function (node, visited, temp, graph, result) {
+  const topologicalSortRecursion = function(node, visited, temp, graph, result) {
     temp[node]    = true;
-    var neighbors = graph[node];
-    for (var i = 0; i < neighbors.length; i ++) {
-      var n = neighbors[i];
+    const neighbors = graph[node];
+    for (let i = 0; i < neighbors.length; i ++) {
+      const n = neighbors[i];
       if (temp[n]) {
         throw new Error('The graph is not a DAG');
       }
@@ -26,7 +27,7 @@ function topologicalSort(graph) {
     visited[node] = true;
     result.push(node);
   }
-  for (var node in graph) {
+  for (let node in graph) {
     if (!visited[node] && !temp[node]) {
       topologicalSortRecursion(node, visited, temp, graph, result);
     }
@@ -34,18 +35,68 @@ function topologicalSort(graph) {
   return result.reverse();
 };
 
-/* Uncomment for testing
-var graph = {
-  'A' : ['B', 'C'],
-  'B' : ['D', 'E'],
-  'C' : ['F'],
-  'D' : ['G', 'H'],
-  'E' : [],
-  'F' : ['I', 'J'],
-  'G' : [],
-  'H' : [],
-  'I' : [],
-  'J' : [],
-};
-topologicalSort(graph); 
-*/
+(() => {
+  console.log('Testing started: topological sort');
+  const it = ((description, func) => {
+    console.log(' => ' + description + ' => ' +  func());
+  });
+
+  it('#1 Test correct graph', () => {
+    //      A
+    //    /  \
+    //   B    C
+    //  / \    \
+    // D   E   F 
+    /// \     / \
+    //G H    I   J
+    const adjacencyMatrix = {
+      'A' : ['B', 'C'],
+      'B' : ['D', 'E'],
+      'C' : ['F'],
+      'D' : ['G', 'H'],
+      'E' : [],
+      'F' : ['I', 'J'],
+      'G' : [],
+      'H' : [],
+      'I' : [],
+      'J' : [],
+    };
+    const expect   = ["A", "C", "F", "J", "I", "B", "E", "D", "H", "G"];
+    const response = topologicalSort(adjacencyMatrix);
+
+    if (JSON.stringify(response) == JSON.stringify(['A', 'C', 'F', 'J', 'I', 'B', 'E', 'D', 'H', 'G'])) {
+      return 'PASSED';
+    }
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+
+  it('#2 Test not a DAG graph (With Cycle)', () => {
+    //      A -\
+    //    /  \  \
+    //   B    C  \
+    //  / \    \  \
+    // D   E   F   \ 
+    /// \     / \   \
+    //G H    I   J -/
+    const adjacencyMatrix = {
+      'A' : ['B', 'C'],
+      'B' : ['D', 'E'],
+      'C' : ['F'],
+      'D' : ['G', 'H'],
+      'E' : [],
+      'F' : ['I', 'J'],
+      'G' : [],
+      'H' : [],
+      'I' : [],
+      'J' : ['A'],
+    };
+
+    try {
+      topologicalSort(adjacencyMatrix);
+	  return 'FAILED; EXPECT error';
+    }
+    catch(error) {
+      return 'PASSED';
+    }
+  });
+})();
