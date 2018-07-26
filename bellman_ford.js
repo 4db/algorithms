792a8d1565b4cf5d80efb0232623b5b9fe025266
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-07-26 13:13:35 -0700

    Update bellman_ford.js

--- bellman_ford.js
+++ bellman_ford.js
@@ -1,26 +1,44 @@
+'use sctrict';
+
 /*
  * Weighted graph
  * vertices - array
  * edges - array
  */
-function Graph() {
-  var vm           = this;
-  vm.vertices      = [];
-  vm.edges         = [];
+class Graph {
 
-  vm.addVertex = function(vertex) {
-    vm.vertices.push(vertex);
-  };
+  constructor() {
+    this.vertices = [];
+    this.edges    = [];  
+  }
 
-  vm.addEdge = function(startVertex, endVertex, weight) {
-    var edgeIndex = vm.findEdge(startVertex, endVertex);
+  /**
+   * @param {string|int} vertex
+   */
+  addVertex(vertex) {
+    this.vertices.push(vertex);
+  }
+
+  /**
+   * @param {string|int} startVertex
+   * @param {string|int} endVertex
+   * @param {string|int} weight
+   */
+  addEdge(startVertex, endVertex, weight) {
+    const edgeIndex = this.findEdge(startVertex, endVertex);
     if (edgeIndex !== false) {
-      vm.edges.splice(edgeIndex, 1);
+      this.edges.splice(edgeIndex, 1);
     }
-    vm.edges.push(vm.setEdge(startVertex,endVertex,weight));
-  };
+    this.edges.push(this.setEdge(startVertex,endVertex,weight));
+  }
 
-  vm.setEdge = function(startVertex, endVertex, weight) {
+  /**
+   * @param {[type]} startVertex
+   * @param {[type]} endVertex
+   * @param {[type]} weight
+   * @return {object}
+   */
+  setEdge(startVertex, endVertex, weight) {
     return {
       'startVertex' : startVertex,
       'endVertex'   : endVertex,
@@ -28,11 +46,15 @@ function Graph() {
     };
   }
 
-  vm.findEdge = function(startVertex, endVertex) {
-    for (var i = 0; i< vm.edges.length; i++) {
-      if (
-          (vm.edges[i].startVertex === startVertex && vm.edges[i].endVertex === endVertex) ||
-          (vm.edges[i].endVertex === startVertex && vm.edges[i].startVertex === endVertex)
+  /**
+   * @param  {[type]} startVertex
+   * @param  {[type]} endVertex
+   * @return {boolean|int}
+   */
+  findEdge(startVertex, endVertex) {
+    for (let i = 0; i< this.edges.length; i++) {
+      if ((this.edges[i].startVertex === startVertex && this.edges[i].endVertex === endVertex) ||
+          (this.edges[i].endVertex === startVertex && this.edges[i].startVertex === endVertex)
       ) {
         return i;
       }
@@ -40,97 +62,132 @@ function Graph() {
     return false;
   }
 
-  vm.removeEdge = function(startVertex, endVertex) {
-    var index = vm.findEdge(startVertex, endVertex);
+  /**
+   * @param  {string|int} startVertex
+   * @param  {string|int} endVertex
+   * @return {boolean}
+   */
+  removeEdge(startVertex, endVertex) {
+    const index = this.findEdge(startVertex, endVertex);
     if (index === false) {
-      console.log('Edge with vertexs(' + startVertex + ', ' + endVertex + ') not found');
       return false;
     }
-    vm.edges.splice(index, 1);
-  };
+    this.edges.splice(index, 1);
+    return true;
+  }
 
-  vm.removeVertex = function(vertex) {
-    var index = vm.vertices.indexOf(vertex);
+  /**
+   * @param  {[type]} vertex
+   * @return {[type]}
+   */
+  removeVertex(vertex) {
+    const index = this.vertices.indexOf(vertex);
     if (index !== -1) {
-      vm.vertices.splice(index, 1);
+      this.vertices.splice(index, 1);
     }
-    for (var i = 0; i < vm.vertices.length; i++) {
-      vm.removeEdge(vm.vertices[i], vertex);
+    for (let i = 0; i < this.vertices.length; i++) {
+      this.removeEdge(this.vertices[i], vertex);
     }
-  };
+  }
 
-  vm.getVertexKey = function(vertex) {
-    return vm.vertices.indexOf(vertex);
+  /**
+   * @param  {[type]} vertex
+   * @return {[void]}
+   */
+  getVertexKey(vertex) {
+    return this.vertices.indexOf(vertex);
   }
 }
 
-function bellmanFord(graph, startVertex) {
-  var distances = {};
-  var previousVertices = {};
+/**
+ * @param  {[type]} graph
+ * @param  {[type]} source
+ * @return {object}
+ */
+function bellmanFord(graph, source) {
+  const distances = {};
+  const previousVertices = {};
 
   // Init all distances with infinity assuming that currently we can't reach
   // any of the vertices except start one.
-  distances[startVertex] = 0;
+  distances[source] = 0;
   graph.vertices.map(function(vertex){
     previousVertices[vertex] = null;
-    if (vertex !== startVertex) {
+    if (vertex !== source) {
       distances[vertex] = Infinity;
     }
-    });
+  });
 
-  // We need (|V| - 1) iterations.
-  //for (let iteration = 0; iteration < (graph.vertices.length - 1); iteration++) {
-    // During each iteration go through all vertices.
-    for (var vertex in distances) {
-    //console.log('Go through all vertex edges.', vertex)
-      // Go through all vertex edges.
-      // TODO make edges Neighbors hash
-      for (var i = 0; i < graph.edges.length; i++) {
-        if (graph.edges[i].startVertex == vertex || graph.edges[i].endVertex == vertex) {
-          var neighbor = graph.edges[i].startVertex == vertex ? graph.edges[i].endVertex : graph.edges[i].startVertex;
-          //console.log('// Find out neighbor', neighbor)
-          // Find out if the distance to the neighbor is shorter in this iteration
-          // then in previous one.
-          var distanceToVertex = distances[vertex];
-          var distanceToNeighbor = distanceToVertex + graph.edges[i].weight;
-          console.log(distanceToVertex, distanceToNeighbor, 'distanceToVertex, distanceToNeighbor')
-          if (distanceToNeighbor < distances[neighbor]) {
-          //console.log(distances)
-            distances[neighbor] = distanceToNeighbor;
-            previousVertices[neighbor] = vertex;
-          }
+  for (let vertex in distances) {
+    for (let i = 0; i < graph.edges.length; i++) {
+      if (graph.edges[i].startVertex == vertex || graph.edges[i].endVertex == vertex) {
+        const neighbor = graph.edges[i].startVertex == vertex ? graph.edges[i].endVertex : graph.edges[i].startVertex;
+
+        // Find out if the distance to the neighbor is shorter in this iteration
+        // then in previous one.
+        const distanceToVertex = distances[vertex];
+        const distanceToNeighbor = distanceToVertex + graph.edges[i].weight;
+
+        if (distanceToNeighbor < distances[neighbor]) {
+          distances[neighbor] = distanceToNeighbor;
+          previousVertices[neighbor] = vertex;
         }
       }
     }
-  
+  }
   return {distances, previousVertices};
 }
 
 
-var graph = new Graph();
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('F');
+(() => {
+  console.log('Testing started: depth-first search traversal');
+  const it = ((description, func) => {
+    console.log(' => ' + description + ' => ' +  func());
+  });
 
-graph.addEdge('A', 'B', 1);
-graph.addEdge('B', 'D', 2);
+  /*
+   * A - (1) --- B  - (5)- F
+   * | \         |        /
+   *(3) -(10)-  (2)   (1) 
+   * |         \ |   /
+   * C - (-2) -- D -
+   */
+  const graph = new Graph();
+  graph.addVertex('A');
+  graph.addVertex('B');
+  graph.addVertex('C');
+  graph.addVertex('D');
+  graph.addVertex('F');
 
-graph.addEdge('A', 'D', 10);
+  graph.addEdge('A', 'B', 1);
+  graph.addEdge('B', 'D', 2);
 
-graph.addEdge('A', 'C', 3);
-graph.addEdge('C', 'D', -2);
+  graph.addEdge('A', 'D', 10);
 
-graph.addEdge('B', 'F', 5);
-graph.addEdge('D', 'F', 1);
+  graph.addEdge('A', 'C', 3);
+  graph.addEdge('C', 'D', -2);
 
-console.log(graph);
-/*
- * A - (1) --- B  - (5)- F
- * | \         |        /
- *(3) -(10)-  (2)   (1) 
- * |         \ |   /
- * C - (-2) -- D -
- */
-bellmanFord(graph, 'A');
+  graph.addEdge('B', 'F', 5);
+  graph.addEdge('D', 'F', 1);
+
+  it('#1 Test shortest path to F ', () => {
+    const input    = 'A';
+    const expect   = 2;
+    const response = bellmanFord(graph, input).distances;
+
+    if (expect === response.F) {
+      return 'PASSED';
+    }
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+
+  it('#2 Test path to C', () => {
+    const input    = 'A';
+    const expect   = -1;
+    const response = bellmanFord(graph, input).distances;
+    if (expect === response.C) {
+      return 'PASSED';
+    }
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+})();
