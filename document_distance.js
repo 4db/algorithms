1a313ea97759cd38c5bcd4e8d41defea4547642f
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-08-08 22:08:24 -0700

    Update document_distance.js

--- document_distance.js
+++ document_distance.js
@@ -1,43 +1,61 @@
-function DocumentDistance() {
-	var vm    = this;
-	vm.vec1   = null;
-	vm.vec2   = null;
-	vm.text1  = null;
-	vm.text2  = null;
+class DocumentDistance {
+  constructor() {
+    this.vec1   = null;
+    this.vec2   = null;
+    this.text1  = null;
+    this.text2  = null;
+  }
 
-	vm.setVector = function(text) {
-		var obj = {};
-		text.split(' ').map(function(w) {
-			obj[w] = obj[w] ? obj[w] + 1 : 1;
-		});
-		return obj;
+  setVector(text) {
+    let obj = {};
+    text.split(' ').map((w) => {
+      obj[w] = obj[w] ? obj[w] + 1 : 1;
+    });
+    return obj;
+  }
+
+  getConsine(text1, text2) {
+    this.vec1 = this.setVector(text1);
+    this.vec2 = this.setVector(text2);
+    let numerator = 0;
+    this.intersection(this.vec1, this.vec2).map((word) => {
+      numerator =numerator + (this.vec1[word] * this.vec2[word]);
+    });
+
+    let denominator = Math.sqrt(this.getSum(this.vec1)) * Math.sqrt(this.getSum(this.vec2))
+
+    return !denominator ? 0.0 : numerator / denominator;
+  }
+
+  getSum(obj) {
+    let sum = 0;
+    for (let x in obj) {
+      sum = sum + obj[x] ** 2;
     }
-	
-	vm.getConsine = function(text1, text2) {
-		vm.vec1 = vm.setVector(text1);
-		vm.vec2 = vm.setVector(text2);
-		var numerator    = 0;
-		vm.intersection(vm.vec1, vm.vec2).map(function(word) {
-			numerator =numerator + (vm.vec1[word] * vm.vec2[word]);
-		});
+    return sum;
+  }
 
-		var denominator = Math.sqrt(vm.getSum(vm.vec1)) * Math.sqrt(vm.getSum(vm.vec2))
-
-		return !denominator ? 0.0 : numerator / denominator;
-    }
-
-	vm.getSum = function(obj) {
-		var sum = 0;
-		for (x in obj) {
-			sum = sum + obj[x]**2;
-        }
-		return sum;
-	}
-
-    vm.intersection = function(o1, o2) {
-        return Object.keys(o1).filter({}.hasOwnProperty.bind(o2));
-    }
+  intersection(o1, o2) {
+      return Object.keys(o1).filter({}.hasOwnProperty.bind(o2));
+  }
 }
 
-var dd = new DocumentDistance(); 
-dd.getConsine('This is a foo bar sentence', 'This sentence is similar to a foo bar sentence');
+(() => {
+  console.log('Testing started: Document Distance');
+  const it = ((description, func) => {
+    console.log(' => ' + description + ' => ' +  func());
+  });
+
+  it('#1 Get distance', () => {
+    const docDis   = new DocumentDistance();
+    const input1   = 'This is a foo bar sentence';
+    const input2   = 'This sentence is similar to a foo bar sentence';
+    const expect   = 0.8616404368553293;
+
+    const response = docDis.getConsine(input1, input2);
+    if (expect === response) {
+      return 'PASSED';
+    }
+    return 'FAILED; EXPECT:' + expect + ' !== ' + response;
+  });
+})();
