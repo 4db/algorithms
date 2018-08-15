941b20a8cafb677f23b4d168f8b1a3e93130c08b
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-08-14 21:27:25 -0700

    Create matrix_distances.js

---
+++ matrix_distances.js
@@ -0,0 +1,8 @@
+// Write a functions to find a closest distances in matrix
+// Input
+// dinstances array with x,y coordinate
+// size of search disntaces 
+
+// Example 
+// Input: [[1,2], [1, -1], [1,5]], 2
+// Output: [[1,2], [1, -1]]
