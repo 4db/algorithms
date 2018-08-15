f899ad25404ab5da814f335a3c70a9b658da238b
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-08-14 21:34:34 -0700

    Create double_greedy_algorithm.js

---
+++ double_greedy_algorithm.js
@@ -0,0 +1,18 @@
+// Write a function to search closest distance combination betwe forward distance + return distance
+// Input
+    forwardOptions [10, 8]
+    returnOptions [10, 12, 8]
+    maxDistance 20
+
+//Output [
+{
+    forward : 10
+    return : 10
+}
+{
+    forward : 8
+    return : 12
+}
+]
+
+
