84d6c47f96aa700751ff2c279788f1e78aba86ca
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-08-17 20:56:15 -0700

    Update quick_sort.js

--- quick_sort.js
+++ quick_sort.js
@@ -1,9 +1,19 @@
+/*
+ * @param {array} arr
+ * @param {string|int} key
+ * @param {string|int} secondKey
+ */
 function swap(arr, key, secondKey) {
     var temp = arr[key];
     arr[key] = arr[secondKey];
     arr[secondKey] = temp;
 }
 
+/*
+ * @param {array} arr
+ * @param {array} left
+ * @param {array} right
+ */
 function getPivot(arr, left, right) {
     var pivot = arr[Math.floor((left + right) / 2)];
 
@@ -46,5 +56,11 @@ function quickSort(arr, left, right) {
 
     return arr;
 }
+function it(desc, input, expect) {
+	console.log(desc);
+	console.log(input == expect ? 'PASSED' : 'FAIL');
+}
 
-console.log(quickSort([9,8,6,5,4,3]));
+it ('should sort integer', quickSort([5,4,3,2,1]).join(''), [1,2,3,4,5].join(''));
+it ('should sort negative integer and 0', quickSort([1,2,3,-1,0]).join(''), [-1,0,1,2,3].join(''));
+it ('should sort negative chars', quickSort(['c','b','a']).join(''), ['a','b','c'].join(''));
