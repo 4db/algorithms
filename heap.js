04b53d89e859f428a728116b6ae6cbdac5f50367
Alex Dubinchyk <alexs.dbk@gmail.com>
2018-08-22 23:28:58 -0700

    Update heap.js

--- heap.js
+++ heap.js
@@ -1,1 +1,17 @@
-// Heap.js
+// heappop - pop and return the smallest element from heap
+// heappush - push the value item onto the heap, maintaining
+//             heap invarient
+//heapify - transform list into heap, in place, in linear time
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+  
+  parent(i) {
+    return (i-1)/2;
+  }
+  
+  insertKey(key) {
+    this.heappush(k)
+  }
+}
