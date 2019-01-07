9067ca1acb307abc10ec735b85d85850697fbaca
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-01-06 20:34:52 -0800

    Update SinglyLinkedList.java
    Updated & remove comments

--- java/SinglyLinkedList/SinglyLinkedList.java
+++ java/SinglyLinkedList/SinglyLinkedList.java
@@ -1,12 +1,12 @@
 public class SinglyLinkedList {
-    private Node head;  // Head is first node in linked list
+    private Node head;
 
     public boolean isEmpty(){
         return length() == 0;
     }
  
     public void append(String data){
-        if(head == null){
+        if (head == null) {
             head = new Node(data);
             return;
         }
@@ -15,9 +15,8 @@ public class SinglyLinkedList {
  
     private Node tail() {
         Node tail = head;
-     
-        // Find last element of linked list known as tail
-        while(tail.next != null){
+
+        while(tail.next != null) {
             tail = tail.next;
         }      
         return tail;
@@ -26,28 +25,12 @@ public class SinglyLinkedList {
  
     public int length() {
        int length = 0;
-       Node current = head;  // Starts counting from head - first node
-       while(current != null){
+       Node current = head;
+
+       while(current != null) {
            length ++;
            current = current.next;
        }
        return length;
-    }
-
- 
-    // Node is nested class because it only exists along with linked list
-    // Node is private because it's implementation detail
-    private static class Node {
-        private Node next;
-        private String data;
-
-        public Node(String data) {
-            this.data = data;
-        }
-
-        @Override
-        public String toString() {
-            return this.data;
-        }
     }
 }
