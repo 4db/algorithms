3f10be871f42a0cf27242cd9bcec78c7b6a1803e
aldb <alexs.dbk@gmail.com>
2019-02-05 21:44:20 -0800

    Add BFS

---
+++ java/BFS/Graph.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * Graph class represents a directed graph using adjacency list
+ */
+class Graph {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    Graph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    void addEdge(int v,int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+        boolean visited[] = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            s = queue.poll();
+
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+}
