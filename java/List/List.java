dcaf4c35279cea975f83805797fa4174ff26bc4c
aldb <alexs.dbk@gmail.com>
2019-02-06 23:04:09 -0800

    Add list

---
+++ java/List/List.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+/*
+ * List represents a data structure which allows to dynamically add,
+ * access and remove objects of the same type
+ */
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = e;
+    }
+
+    private void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    @SuppressWarnings('unchecked')
+    public E get(int i) {
+        if (i>= size || i <0) {
+            throw new IndexOutOfBoundsException('Index: %s, Size %d', i, size);
+        }
+        return (E) elements[i];
+    }
+}
