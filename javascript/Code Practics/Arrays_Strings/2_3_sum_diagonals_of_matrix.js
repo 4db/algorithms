d8077c981ba58bec90f6115dbe553d2ea01c7730
aldb <alexs.dbk@gmail.com>
2019-02-22 21:26:16 -0800

    Add diagonals in matrix

---
+++ javascript/Code Practics/Arrays_Strings/2_3_sum_diagonals_of_matrix.js
@@ -0,0 +1,20 @@
+/* Given a square matrix, calculate the absolute difference between
+ * the sums of its diagonals.
+ */
+
+function diagonalDifference(arr) {
+  let leftD = 0;
+  let rightD = 0;
+  for (let i = 0; i < arr.length; i++) {
+    leftD += arr[i][i];
+    rightD += arr[i][arr.length - 1 - i];
+  }
+  return Math.abs(leftD - rightD);
+}
+
+const demo = [
+  [11,2,4],
+  [4,5,6],
+  [10,8, -12]
+];
+console.log('It should return 15', diagonalDifference(demo) === 15 ? 'PASS' : 'FAIL');
