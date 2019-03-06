3b74081607391fdd316581a1ab6e7d522fab74f5
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-03-05 21:29:17 -0800

    Create 1.2_clone_graph.js

---
+++ javascript/Code Practics/Graphs/1.2_clone_graph.js
@@ -0,0 +1,48 @@
+/**
+ * Given a reference of a node in a connected undirected graph,
+ * return a deep copy (clone) of the graph. Each node in the graph contains
+ * a val (int) and a list (List[Node]) of its neighbors.
+ * // Definition for a Node.
+ * function Node(val,neighbors) {
+ *    this.val = val;
+ *    this.neighbors = neighbors;
+ * };
+ */
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+const UndirectedGraphNode = function(v, n = []) {
+    this.val = v;
+    this.neighbors = n;
+};
+
+// use queue and breadth first search
+const cloneGraph = function(graph) {
+  if (graph === null) {
+    return graph;
+  }
+
+  const queue = [graph];
+
+  // make initial copy
+  const rootCopy = new UndirectedGraphNode(graph.val);
+  const mapping = {};
+  mapping[graph.val] = rootCopy;
+
+  while (queue.length > 0) {
+    let currNode = queue.shift();
+
+    currNode.neighbors.forEach((neighbor) => {
+      if (!mapping[neighbor.val]) {
+        let newNode = new UndirectedGraphNode(neighbor.val);
+        mapping[neighbor.val] = newNode;
+        queue.push(neighbor);
+      }
+
+      mapping[currNode.val].neighbors.push(mapping[neighbor.val]);
+    });
+  }
+
+  return rootCopy;
+};
