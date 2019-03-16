2ebb3b6915be8119d3bfcb3a6ca151bc37548984
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-03-16 11:56:39 -0700

    Update 2_0_network_delay_time.js

--- javascript/Code Practics/Graphs/2_0_network_delay_time.js
+++ javascript/Code Practics/Graphs/2_0_network_delay_time.js
@@ -1,27 +1,47 @@
-/*743. Network Delay Time*/
+/**
+ * 743. Network Delay Time
+ * There are N network nodes, labelled 1 to N.
+ * Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node,
+ * v is the target node, and w is the time it takes for a signal to travel from source to target.
+ * Now, we send a signal from a certain node K. How long will it take for all nodes to receive the signal?
+ * If it is impossible, return -1.
+ **/
+
+/*
+ * @param {void} val
+ */
 function Node(val) {
-    this.val = val;
-    this.next = [];
-    this.time = Infinity;
+  this.val = val;
+  this.next = [];
+  this.time = Infinity;
 }
 
+/*
+ * @param {array} list
+ * @return {element}
+ */
 function popSmallestTime(list) {
-    list.sort((a, b) => a.time - b.time);
-    return list.shift();
+  list.sort((a, b) => a.time - b.time);
+  return list.shift();
 }
 
+/*
+ * @param {number} numNodes
+ * @param {number[][]} edges
+ * @return {graph}
+ */
 function createGraph(numNodes, edges) {
-    const nodes = {};
-    
-     for(var i = 1; i <= numNodes; i++) {
-        nodes[i] = new Node(i);
-    }
-    
-    edges.forEach(([u, v, w]) => {
-        nodes[u].next.push([nodes[v], w]);
-    });
-    
-    return nodes;
+  const nodes = {};
+
+  for(var i = 1; i <= numNodes; i++) {
+    nodes[i] = new Node(i);
+  }
+
+  edges.forEach(([u, v, w]) => {
+    nodes[u].next.push([nodes[v], w]);
+  });
+
+  return nodes;
 }
 
 /**
@@ -30,25 +50,25 @@ function createGraph(numNodes, edges) {
  * @param {number} K
  * @return {number}
  */
-var networkDelayTime = function(times, N, K) {
-    // we want to basically run djikstra and keep
-    // track of the max time as we visit a node
-    
-    const nodes = createGraph(N, times);
-    const unvisited = Object.values(nodes);
-    nodes[K].time = 0;
-    let maxTime = 0;
-    
-    do {
-        const node = popSmallestTime(unvisited); 
-        maxTime = node.time;
-        node.next.forEach(([next, weight]) => {
-            if ((node.time + weight) < next.time) {
-                next.time = node.time + weight;
-            }
-        });
-    } while (unvisited.length);
-    
-    // if the max time is Infinity it means that node wasn't visited at all from the source
-    return maxTime === Infinity ? -1 : maxTime;
-};
+function networkDelayTime(times, N, K) {
+  // Run djikstra and keep track of the max time as we visit a node
+  const nodes = createGraph(N, times);
+  const unvisited = Object.values(nodes);
+  nodes[K].time = 0;
+  let maxTime = 0;
+
+  while (unvisited.length) {
+    const node = popSmallestTime(unvisited); 
+    maxTime = node.time;
+    node.next.forEach(([next, weight]) => {
+      if ((node.time + weight) < next.time) {
+          next.time = node.time + weight;
+      }
+    });
+  }
+
+  // if the max time is Infinity it means that node wasn't visited at all from the source
+  return maxTime === Infinity ? -1 : maxTime;
+}
+
+console.log('It should return 2', networkDelayTime([[2,1,1],[2,3,1],[3,4,1]], 4, 2) === 2 ? 'PASS' : 'FAIL');
