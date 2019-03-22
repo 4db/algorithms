a641bf2704cce7dcee626389890043057f1a7f20
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-03-21 17:06:46 -0700

    Update 1_4_Find_N_node_from_end.js

--- javascript/Code Practics/Linked_Lists/1_4_Find_N_node_from_end.js
+++ javascript/Code Practics/Linked_Lists/1_4_Find_N_node_from_end.js
@@ -17,27 +17,28 @@ function Node(nextNode, value) {
  * @return {number|string}
  */
 function getNElValue(frontNode, target) {
+  if (target < 1) {
+    return -1;
+  }
   let count = 1;
   let current = frontNode.next;
+  const out = [];
 
   while (current.next) {
-  current = current.next;
+    current = current.next;
     count++;
-  }
-
-  current = frontNode.next;
-  while (current.next) {
-    if (count === target) {
-      return current.value;
+    out.push(current.value);
+    if (count-1 > target) {
+      out.shift();
     }
-  current = current.next;
-    count--;
   }
-  return -1;  
+  
+  return target > count ? -1 : out.shift();  
 }
 
 const linkedList = new Node(null, -1);
 [1,2,3,4,5,6,7,8,9,10].reverse().map(el => linkedList.next = new Node(linkedList.next, el));
-console.log('It should return 8', getNElValue(linkedList, 3) === 8 ? 'PASS' : 'FAIL');
+console.log('It should return 8', getNElValue(linkedList, 3), getNElValue(linkedList, 3) === 8 ? 'PASS' : 'FAIL');
 console.log('It should return 2', getNElValue(linkedList, 9) === 2 ? 'PASS' : 'FAIL');
 console.log('It should return -1', getNElValue(linkedList, 99) === -1 ? 'PASS' : 'FAIL');
+console.log('It should return -1', getNElValue(linkedList, -1) === -1 ? 'PASS' : 'FAIL');
