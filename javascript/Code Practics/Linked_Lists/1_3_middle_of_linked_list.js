6c830502e07be2d3ca33c88da30bd3a6b772a175
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-03-31 21:12:28 -0700

    Update 1_3_middle_of_linked_list.js
    Fix test case

--- javascript/Code Practics/Linked_Lists/1_3_middle_of_linked_list.js
+++ javascript/Code Practics/Linked_Lists/1_3_middle_of_linked_list.js
@@ -1,14 +1,14 @@
 /**
  * 876. Middle of the Linked List
  */
- 
- /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+/*
+ * @param {Node|null} nextNode
+ * @param {value} value
  */
+function Node(nextNode, value) {
+  this.next = nextNode;
+  this.value = value;
+}
 /**
  * @param {ListNode} head
  * @return {ListNode}
@@ -22,5 +22,7 @@ function middleNode(head) {
   }
   return slow
 }
+const TEST_LINKED_LIST = new Node(null, -1);
+[1,2,3,4,5,6,7,8,9,10].reverse().map(el => TEST_LINKED_LIST.next = new Node(TEST_LINKED_LIST.next, el));
 
-//console.log('It should return [3,4,5]', middleNode([1,2,3,4,5]) == [3,4,5] ? 'PASS' : 'FAIL');
+console.log('It should return 5', middleNode(TEST_LINKED_LIST).value === 5 ? 'PASS' : 'FAIL');
