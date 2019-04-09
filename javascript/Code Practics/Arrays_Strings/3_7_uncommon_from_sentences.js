54f68a0cf45e50ef3de245d3b1415820bd752aae
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-04-08 21:30:35 -0700

    Create 3_7_uncommon_from_sentences.js

---
+++ javascript/Code Practics/Arrays_Strings/3_7_uncommon_from_sentences.js
@@ -0,0 +1,27 @@
+/**
+ * 884. Uncommon Words from Two Sentences
+ *
+ * We are given two sentences A and B. A word is uncommon 
+ * if it appears exactly once in one of the sentences,
+ * and does not appear in the other sentence.
+ * Return a list of all uncommon words. 
+ *
+ * @param {string} A
+ * @param {string} B
+ * @return {string[]}
+ */
+function uncommonFromSentences(A, B) {
+  A = A.split(' ');
+  B = B.split(' ');
+  const isUnCommon = w => {
+    return (A.includes(w) && !B.includes(w)
+     && A.indexOf(w) === A.lastIndexOf(w))
+    ||
+    (B.includes(w) && !A.includes(w)
+    && B.indexOf(w) === B.lastIndexOf(w));
+  }
+  return [... new Set(A.concat(B))].filter(w => isUnCommon(w));
+}
+
+console.log('It should return ["sweet","sour"]', uncommonFromSentences("this apple is sweet",
+"this apple is sour"));
