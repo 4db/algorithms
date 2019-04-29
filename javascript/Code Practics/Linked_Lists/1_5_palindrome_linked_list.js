8b6a725ba85ca12d9c6bc808be1c990af4ec61ec
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-04-28 23:29:15 -0700

    Updated comments

--- javascript/Code Practics/Linked_Lists/1_5_palindrome_linked_list.js
+++ javascript/Code Practics/Linked_Lists/1_5_palindrome_linked_list.js
@@ -1,6 +1,5 @@
 /**
  * 234. Palindrome Linked List
- *
  * @param {ListNode} head
  * @return {boolean}
  */
@@ -8,27 +7,26 @@ function isPalindrome(head) {
   if (head === null || head.next === null) {
     return true;
   }
-
   let walker = head;
   let runner = head;
   let pre = null;
   while(runner !== null && runner.next !== null) {
-    //move runner first, otherwise the next of the head will be already changed
+    // move runner first, otherwise the next of the head will be already changed
     runner = runner.next.next;
-    //reverse linked list
+    // reverse linked list
     nextWalker = walker.next;
     walker.next = pre;
     pre = walker;
     walker = nextWalker;
   }
 
-  // after this the pre would be the head of our first half, while walker at the head of second half
-  // odd number length
+  // after this the pre would be the head of our first half, 
+  // while walker at the head of second half odd number length
   if (runner != null) {
     walker = walker.next;
   }
   while (pre !== null && walker !== null) {
-    if(pre.val != walker.val) {
+    if (pre.val !== walker.val) {
       return false;
     }
     pre = pre.next;
