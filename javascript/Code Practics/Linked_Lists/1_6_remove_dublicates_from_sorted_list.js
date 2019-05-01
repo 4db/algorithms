749cac43aa16879492630b0c5cf2cc5929b2b8c3
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-04-30 20:23:47 -0700

    Added extra test case

--- javascript/Code Practics/Linked_Lists/1_6_remove_dublicates_from_sorted_list.js
+++ javascript/Code Practics/Linked_Lists/1_6_remove_dublicates_from_sorted_list.js
@@ -30,4 +30,8 @@ function Node(nextNode, val) {
 
 const TEST_LINKED_LIST = new Node(null, 1);
 [1,1,1,1,2].reverse().map(el => TEST_LINKED_LIST.next = new Node(TEST_LINKED_LIST.next, el));
-console.log('It should return next 2', deleteDuplicates(TEST_LINKED_LIST).next === 2);
+console.log('It should return next 2', deleteDuplicates(TEST_LINKED_LIST).next.val === 2 ? 'PASS' : 'FAIL');
+
+const TEST_LINKED_LIST_2 = new Node(null, 3);
+[3,3,4].reverse().map(el => TEST_LINKED_LIST_2.next = new Node(TEST_LINKED_LIST_2.next, el));
+console.log('It should return next 3', deleteDuplicates(TEST_LINKED_LIST_2).val === 3 ? 'PASS' : 'FAIL');
