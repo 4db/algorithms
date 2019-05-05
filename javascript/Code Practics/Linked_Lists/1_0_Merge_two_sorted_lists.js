843bd88499320c64fd77bf7434946185cb5f93ec
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-05-04 21:57:25 -0700

    Added test case

--- javascript/Code Practics/Linked_Lists/1_0_Merge_two_sorted_lists.js
+++ javascript/Code Practics/Linked_Lists/1_0_Merge_two_sorted_lists.js
@@ -2,14 +2,7 @@
  * Merge two sorted linked lists and return it as a
  * new list. The new list should be made by splicing
  * together the nodes of the first two lists.
- * Definition for singly-linked list.
- * function ListNode(val) {
- *   this.val = val;
- *   this.next = null;
- * }
- */
-
-/**
+ *
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
@@ -33,3 +26,20 @@ function mergeTwoLists(l1, l2) {
   runner.next = l1 || l2;
   return mergedLinkedListHead.next;
 }
+
+/*
+ * @param {Node|null} nextNode
+ * @param {value} value
+ */
+function Node(nextNode, value) {
+  this.next = nextNode;
+  this.value = value;
+}
+
+const TEST_LINKED_LIST_1 = new Node(null, -1);
+[1,2,3,4,5].reverse().map(el => TEST_LINKED_LIST_1.next = new Node(TEST_LINKED_LIST_1.next, el));
+
+const TEST_LINKED_LIST_2 = new Node(null, -1);
+[6,7,8,9,10].reverse().map(el => TEST_LINKED_LIST_2.next = new Node(TEST_LINKED_LIST_2.next, el));
+
+console.log('It should return merged linked list', conmergeTwoLists(TEST_LINKED_LIST_1, TEST_LINKED_LIST_2));
