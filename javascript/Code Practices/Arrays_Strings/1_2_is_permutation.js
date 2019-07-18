1d769265e693c1e05900de4d5ea284f92923168b
4db <alexs.dbk@gmail.com>
2019-07-17 21:12:41 -0700

    Add test case

--- javascript/Code Practices/Arrays_Strings/1_2_is_permutation.js
+++ javascript/Code Practices/Arrays_Strings/1_2_is_permutation.js
@@ -8,7 +8,7 @@
  * sensitive and whitespace is significant. So, "god   " is different from "dog".
  *
  * Time Complexity O(N log(N) / N2 (For Quick Sort worse case))
- * Space Complexity TradeOff: Quick Sort O(log(N)) or Merge SortO(N)
+ * Space Complexity TradeOff: Quick Sort O(log(N)) or Merge Sort O(N)
  *
  * @param string str1
  * @param string str2
@@ -23,26 +23,66 @@ function isPermutation(str1, str2) {
   return str1 === str2;
 }
 
-/*
- * TODO
- * For ASCII characters set, only 128 unique characters
- *
- * Time Complexity O(128 -> 1)
- * Space Complexity O(N)
- *
- * @param string str
- * @return boolean
- */
-function isPermutationTradeOff(str1, str2) {
-  return true;
-}
-
 (() => {
   const test = 'Test isPermutation ';
   const it = ((description, str1, str2, expect) => {
     console.log(
       test + description + ': ',
         isPermutation(str1, str2) === expect 
+        ? 
+        'PASS'
+        : 'FAIL'
+    );
+  });
+  it('should return true for permutations combination', 'god', 'dog', true);
+  it('should return false for not permutations combination', 'test', 'asdf', false);
+  it('should return true for permutations combination with upper Chars', 'CiViC', 'civic', true);
+  it('should return false for not permutations combination with upper Chars', 'asdfAsd', 'FAdfAsd', false);
+})();
+
+/*
+ * For ASCII characters set, only 128 unique characters.
+ *
+ * Time Complexity O(3N -> N)
+ * Space Complexity O(256 -> 128 -> 1)
+ * In this example used only ABC set O(26 -> 1)
+ *
+ * @param string str
+ * @return boolean
+ */
+function isPermutationTradeOff(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  // assume only ABC characters set
+  let memo = {};
+  'abcdefghijklmnopqrstuvwxyz'.split('').map(c => memo[c] = 0);
+
+  for (let i = 0; i < str1.length; i++) {
+    const ch = str1[i].toLowerCase();
+    memo[ch]++;
+  }
+  for (let i = 0; i < str2.length; i++) {
+    const ch = str2[i].toLowerCase();
+    if (memo[ch] === 0) {
+      return false;
+    }
+    memo[ch]--;
+  }
+  for (let ch in memo) {
+    if (memo[ch] !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+(() => {
+  const test = 'Test isPermutationTradeOff ';
+  const it = ((description, str1, str2, expect) => {
+    console.log(
+      test + description + ': ',
+        isPermutationTradeOff(str1, str2) === expect 
         ? 
         'PASS'
         : 'FAIL'
