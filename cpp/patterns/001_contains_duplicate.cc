52d76c704d1550e58068909e80e6c73f84a7a076
Alex Dubinchyk <hi.alexd@gmail.com>
2021-12-31 17:54:50 -0800

    Create 001_contains_duplicate.cc
    Create a new folder and add cpp solution. Happy new year!

---
+++ cpp/patterns/001_contains_duplicate.cc
@@ -0,0 +1,19 @@
+// This is solition for https://leetcode.com/problems/contains-duplicate, e.x:
+// Given an int array nums, return true if any value appears at least twice in the array.
+class Solution {
+public:
+    // std::unordered_set(On) faster then std::set(OlogN).
+    bool containsDublicateOneLine(vector<int>& nums) {
+      return nums.size() > unordered_set<int>(nums.begin(), nums.end()).size();
+    }
+    // Faster solution without extra memory.
+    bool containsDuplicate(vector<int>& nums) {      
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i+1] == nums[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
