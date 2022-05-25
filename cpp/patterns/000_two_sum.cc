465153068f799d4cf02b4de543062fc49635d4b2
Alex Dubinchyk <hi.alexd@gmail.com>
2022-05-24 23:46:54 -0700

    Update 000_two_sum.cc
    Remove unused vector.

--- cpp/patterns/000_two_sum.cc
+++ cpp/patterns/000_two_sum.cc
@@ -8,7 +8,7 @@ public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         //Key is the number and value is its index in the vector.
         unordered_map<int, int> hash;
-        vector<int> result;
+
         for (int i = 0; i < numbers.size(); i++) {
             int numberToFind = target - numbers[i];
 
