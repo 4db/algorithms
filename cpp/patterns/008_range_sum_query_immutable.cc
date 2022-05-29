365f0bc0b02f25a62c985a43ceab9516bb2686f5
Alex Dubinchyk <hi.alexd@gmail.com>
2022-05-28 22:57:12 -0700

    Create 008_range_sum_query_immutable.cc

---
+++ cpp/patterns/008_range_sum_query_immutable.cc
@@ -0,0 +1,29 @@
+// Leetcode 303. Range Sum Query - Immutable.
+// Given an integer array nums, handle multiple queries of the following type:
+// Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Input
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// Output
+// [null, 1, -1, -3]
+
+class NumArray {
+public:
+    NumArray(vector<int> &nums) {
+        accu.push_back(0);
+        for (int num : nums)
+            accu.push_back(accu.back() + num);
+    }
+
+    int sumRange(int i, int j) {
+        return accu[j + 1] - accu[i];
+    }
+private:
+    vector<int> accu;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
