bb80bda07a6819e78b1f51fb5140c617669ac8e5
Alex Dubinchyk <hi.alexd@gmail.com>
2022-06-02 19:02:22 -0700

    Create 017_binary_search.cc

---
+++ cpp/patterns/017_binary_search.cc
@@ -0,0 +1,21 @@
+// Leetcode 704. Binary Search.
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int low = 0;
+       int high = nums.size() - 1;
+       while (low <= high) {
+           const int mid = low + (high-low) / 2;
+           if (nums[mid] == target) {
+               return mid;
+           }
+           if (nums[mid] > target) {
+               high = mid - 1;
+           }
+           else {
+               low = mid +1;
+           }
+       }
+        return -1;
+    }
+};
