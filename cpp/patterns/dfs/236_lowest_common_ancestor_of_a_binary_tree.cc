ab43f54790007105b42315225fb290d37a2aebff
Alex Dubinchyk <hi.alexd@gmail.com>
2022-06-17 18:01:24 -0700

    Update 236_lowest_common_ancestor_of_a_binary_tree.cc

--- cpp/patterns/dfs/236_lowest_common_ancestor_of_a_binary_tree.cc
+++ cpp/patterns/dfs/236_lowest_common_ancestor_of_a_binary_tree.cc
@@ -19,3 +19,24 @@ public:
     return p1 && p2 ? root : p1 ? p1 : p2;
     }
 };
+
+class EasyToUnderstandSolution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return dfsTraverse(root, p, q);
+    }
+    
+    TreeNode* dfsTraverse(TreeNode * root, TreeNode * p , TreeNode * q) {
+        if( root == p || root == q || root == NULL) {
+            return root;
+        }
+        TreeNode* parent1 = dfsTraverse(root->left, p, q);
+        TreeNode* parent2 = dfsTraverse(root->right, p, q);
+        if( parent1 && parent2) {
+            return root;
+        }
+        else {
+            return parent1 ? parent1:parent2;
+        }
+    }
+};
