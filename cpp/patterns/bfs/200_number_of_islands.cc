8eb8221b1d7fd955c210d1bb18b38dcc5783ac05
Alex Dubinchyk <hi.alexd@gmail.com>
2022-06-23 19:13:26 -0700

    Update 200_number_of_islands.cc

--- cpp/patterns/bfs/200_number_of_islands.cc
+++ cpp/patterns/bfs/200_number_of_islands.cc
@@ -2,7 +2,7 @@ class Solution {
 public:
     // Time Complexity: O(x*y + x*y) => O(xy).
     // Space Complexity: O(1).
-    int numIslands(vector<vector<char>>& grid) {
+    int numIslands(std::vector<std::vector<char>>& grid) {
         x_size = grid.size();
         y_size = x_size ? grid[0].size() : 0;
         int islands = 0;
@@ -11,7 +11,7 @@ public:
             for (int y = 0; y < y_size; y++) {
                 if (grid[x][y] == '1') {
                     islands++;
-                    dfs(grid, x, y);
+                    bfs(grid, x, y);
                 }
             }
         }
@@ -20,9 +20,9 @@ public:
 private:
     int y_size;
     int x_size;
-    queue<pair<int, int>> q;
+    std::queue<std::pair<int, int>> q;
 
-    void bfs(vector<vector<char>>& grid, int x, int y) {
+    void bfs(std::vector<std::vector<char>>& grid, int x, int y) {
         q.push({x, y});
         grid[x][y]='0';
 
@@ -34,25 +34,25 @@ private:
             // ⬇️ move down.
             if(x+1 < x_size && grid[x+1][y] == '1') {
                 grid[x+1][y] = '0';
-                q.push(make_pair(x+1,y));
+                q.push({x+1, y});
             }
             
             // ⬆️ move up.
             if(x-1 >= 0 && grid[x-1][y] == '1') {
                 grid[x-1][y] = '0';
-                q.push(make_pair(x-1,y));
+                q.push({x-1, y});
             }
 
             // ⬅️ move left.
             if(y-1 >= 0 && grid[x][y-1] == '1') {
                 grid[x][y-1] = '0';
-                q.push(make_pair(x,y-1));
+                q.push({x, y-1});
             }
 
             // ➡️ move right.
             if(y+1 < y_size && grid[x][y+1] == '1') {
                 grid[x][y+1] = '0';
-                q.push(make_pair(x,y+1));
+                q.push({x, y+1});
             }
         }
     }
