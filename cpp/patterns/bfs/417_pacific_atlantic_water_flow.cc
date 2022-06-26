ad93b4014f4c6d1f9f0fbcf6da8e4a9180e7e86d
Alex Dubinchyk <hi.alexd@gmail.com>
2022-06-26 11:59:43 -0700

    Update 417_pacific_atlantic_water_flow.cc

--- cpp/patterns/bfs/417_pacific_atlantic_water_flow.cc
+++ cpp/patterns/bfs/417_pacific_atlantic_water_flow.cc
@@ -1,64 +1,72 @@
-class SolutionBFS {
+class Solution {
 public:
-    std::vector<std::vector<int>> pacificAtlantic(std::vector<std::vector<int>>& mat) {
-        if(!size(mat)) {
+    std::vector<std::vector<int>> pacificAtlantic(std::vector<std::vector<int>>& grid) {
+        if(grid.empty()) {
             return ans;
         }
-        m = size(mat);
-        n = size(mat[0]);
-        pacific = std::vector<std::vector<bool>>(m, std::vector<bool>(n, false));
-        atlantic = pacific;
+        rows_size = size(grid);
+        column_size = size(grid[0]);
+        pacific_grid = std::vector<std::vector<bool>>(rows_size, std::vector<bool>(column_size, false));
+        atlantic_grid = pacific_grid;
 
-        for (int i = 0; i < m; i++) {
-            bfs(mat, pacific, i, 0);
-            bfs(mat, atlantic, i, n - 1);
+        // For loop all rows, for pacific bfs left cells, for atlantic bfs right cells, e.x:
+        // P,*, A
+        // P,*, A
+        // P,*, A
+        for (int i = 0; i < rows_size; i++) {
+            bfs(grid, pacific_grid, i, 0);
+            bfs(grid, atlantic_grid, i, column_size - 1);
         }
-        for (int i = 0; i < n; i++) {
-            bfs(mat, pacific, 0, i);
-            bfs(mat, atlantic, m - 1, i);
+
+        // For loop all columns, for pacific bfs top cells, for atlantic bfs bottom:
+        for (int i = 0; i < column_size; i++) {
+            bfs(grid, pacific_grid, 0, i);
+            bfs(grid, atlantic_grid, rows_size - 1, i);
         }
         return ans;
     }
 
 private:
-    int m, n;
+    int rows_size;
+    int column_size;
     std::vector<std::vector<int>> ans;
-    std::vector<std::vector<bool>> atlantic, pacific;
+    std::vector<std::vector<bool>> atlantic_grid, pacific_grid;
     queue<std::pair<int, int>> q;
 
-    void bfs(std::vector<std::vector<int>>& mat, std::vector<std::vector<bool>>& visited, int i, int j) {
-        q.push({i, j});
+    void bfs(std::vector<std::vector<int>>& grid, std::vector<std::vector<bool>>& visited, int x, int y) {
+        q.push({x, y});
         while(!q.empty()) {
-            std::tie(i, j) = q.front();
+            std::tie(x, y) = q.front();
+            std::cout << "x: " << x << "y: " << y << std::endl;
             q.pop();
-            if (visited[i][j]) {
+            if (visited[x][y]) {
                 continue;
             }
 
-            visited[i][j] = true;
+            visited[x][y] = true;
 
-            if (atlantic[i][j] && pacific[i][j]) {
-                ans.push_back(std::vector<int>{i, j});
+            if (atlantic_grid[x][y] && pacific_grid[x][y]) {
+                ans.push_back(std::vector<int>{x, y});
             }
 
             /*⬇️*/
-            if (i + 1 <  m && mat[i + 1][j] >= mat[i][j]) {
-                q.push({i + 1, j});
+            if (x + 1 <  rows_size && grid[x + 1][y] >= grid[x][y]) {
+                q.push({x + 1, y});
             }
 
             /*⬆️*/
-            if (i - 1 >= 0 && mat[i - 1][j] >= mat[i][j]) {
-                q.push({i - 1, j});
+            if (x - 1 >= 0 && grid[x - 1][y] >= grid[x][y]) {
+                q.push({x - 1, y});
             }
 
             /*➡️*/
-            if (j + 1 <  n && mat[i][j + 1] >= mat[i][j]) {
-                q.push({i, j + 1});
+            if (y + 1 <  column_size && grid[x][y + 1] >= grid[x][y]) {
+                q.push({x, y + 1});
             }
 
             /*⬅️*/
-            if (j - 1 >= 0 && mat[i][j - 1] >= mat[i][j]) {
-                q.push({i, j - 1});
+            if (y - 1 >= 0 && grid[x][y - 1] >= grid[x][y]) {
+                q.push({x, y - 1});
             }
         }
     }
