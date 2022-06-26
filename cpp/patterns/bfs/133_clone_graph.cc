bce613e33aa4e93e68618163e4f3cb49506ce8a5
Alex Dubinchyk <hi.alexd@gmail.com>
2022-06-25 20:45:10 -0700

    Update 133_clone_graph.cc

--- cpp/patterns/bfs/133_clone_graph.cc
+++ cpp/patterns/bfs/133_clone_graph.cc
@@ -22,28 +22,28 @@ class Solution {
 public:
     // declaring map, to check whether we have a copy
     // of node or not and also to store copy.
-    unordered_map<Node* , Node*> mp; 
+    std::unordered_map<Node* , Node*> mp; 
     
     Node* cloneGraph(Node* node) {
-        if(node == NULL) {
+        if (node == NULL) {
             return NULL;
         }
         
         Node* first = new Node(node -> val, {}); // make a copy of first node
         mp[node] = first;
         
-        queue<Node*> q; //For bfs, we create queue
+        std::queue<Node*> q; //For bfs, we create queue
         q.push(node); // push into queue
         
         // until q. empty == false
-        while(q.empty() == false) {
+        while (q.empty() == false) {
             Node* curr = q.front(); // extract front node
             q.pop(); // pop that from queue
             
             // now travel in adjcant
-            for(auto adj: curr -> neighbors) {
+            for (auto adj: curr -> neighbors) {
                 // if not present in map.
-                if(mp.find(adj) == mp.end()) {
+                if (mp.find(adj) == mp.end()) {
                     mp[adj] = new Node(adj -> val, {}); // then create copy.
                     q.push(adj); // push nto the queue
                     
