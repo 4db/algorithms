0f02b2c460f8e28dfba5246de91ce1baec50bb7d
Alex Dubinchyk <hi.alexd@gmail.com>
2022-06-25 20:50:48 -0700

    Create 207_course_schedule.cc

---
+++ cpp/patterns/bfs/207_course_schedule.cc
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, std::vector<std::pair<int, int>>& prerequisites) {
+        graph g = buildGraph(numCourses, prerequisites);
+        std::vector<int> degrees = computeIndegrees(g);
+        for (int i = 0; i < numCourses; i++) {
+            int j = 0;
+            for (; j < numCourses; j++) {
+                if (!degrees[j]) {
+                    break;
+                }
+            }
+            if (j == numCourses) {
+                return false;
+            }
+            degrees[j]--;
+            for (int v : g[j]) {
+                degrees[v]--;
+            }
+        }
+        return true;
+    }
+private:
+    // TODO typedef;
+    typedef std::vector<std::vector<int>> graph;
+    
+    graph buildGraph(int numCourses, vector<pair<int, int>>& prerequisites) {
+        graph g(numCourses);
+        for (auto p : prerequisites) {
+            g[p.second].push_back(p.first);
+        }
+        return g;
+    }
+    
+    std::vector<int> computeIndegrees(graph& g) {
+        std::vector<int> degrees(g.size(), 0);
+        for (auto adj : g) {
+            for (int v : adj) {
+                degrees[v]++;
+            }
+        }
+        return degrees;
+    }
+};
