a19717e874c3023ad0a5f9fc233f04dd5fe47339
Alex Dubinchyk <hi.alexd@gmail.com>
2022-11-19 11:21:14 -0800

    Update 001_contains_duplicate.cc
    Add "std::"

--- cpp/patterns/001_contains_duplicate.cc
+++ cpp/patterns/001_contains_duplicate.cc
@@ -3,11 +3,11 @@
 class Solution {
 public:
     // std::unordered_set(On) faster then std::set(OlogN).
-    bool containsDublicateOneLine(vector<int>& nums) {
+    bool containsDublicateOneLine(std::vector<int>& nums) {
       return nums.size() > std::unordered_set<int>(nums.begin(), nums.end()).size();
     }
     // Faster solution without extra memory.
-    bool containsDuplicate(vector<int>& nums) {      
+    bool containsDuplicate(std::vector<int>& nums) {      
         std::sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size() - 1; i++) {
             if (nums[i+1] == nums[i]) {
