8a7130eb39e4e5a8b9cb76c89f91a6ec1f34426f
Alex Dubinchyk <hi.alexd@gmail.com>
2022-11-21 20:11:26 -0800

    Update 004_single_number.cc
    Add xor solution

--- cpp/patterns/004_single_number.cc
+++ cpp/patterns/004_single_number.cc
@@ -4,6 +4,7 @@
 
 class Solution {
 public:
+    // Not optimal
     int singleNumber(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
         const int len = nums.size();
@@ -13,5 +14,19 @@ public:
             }
         }
         return nums[len -1];
+    }
+};
+
+// USING BITWISE XOR OPERATOR.
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) { 
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
+        
+       // Or one liner:
+       return std::accumulate(nums.begin(), nums.end(), 0, std::bit_xor<int>());
     }
 };
