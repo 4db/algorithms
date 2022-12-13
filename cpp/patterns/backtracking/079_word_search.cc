b6866ede4ab23cb7eb4382f87c116b3d77698ebd
Alex Dubinchyk <hi.alexd@gmail.com>
2022-12-12 21:27:27 -0800

    Update 079_word_search.cc
    Added DFS

--- cpp/patterns/backtracking/079_word_search.cc
+++ cpp/patterns/backtracking/079_word_search.cc
@@ -40,3 +40,50 @@ public:
        return false;
    }
 };
+
+// DFS.
+// Time: O(N+M) => (O(E+V)).
+// Space: O(1).
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        int index = 0;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (board[i][j] == word[index]) {
+                    if (search(i, j, index, board, word)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+    
+    bool search(int i, int j, int index, vector<vector<char>>& board, string& word) {
+        int m = board.size(), n = board[0].size();
+        
+        if (index == word.size()) {
+            return true;
+        }
+        if (i < 0 or j < 0 or i >= m or j >= n) {
+            return false;
+        }
+
+        bool result = false;
+        if (word[index] == board[i][j]) {
+            char temp = word[index];
+            board[i][j] = '#';
+            result = search(i+1, j, index+1, board, word) || search(i-1, j, index+1, board, word) || 
+                search(i, j+1, index+1, board, word) || search(i, j-1, index+1, board, word);
+            
+            board[i][j] = temp;
+        }
+        
+        return result;
+    }
+    
+};
