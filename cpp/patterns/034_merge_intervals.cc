6da138b49d5e897a6835c317426dc841251db781
Alex Dubinchyk <hi.alexd@gmail.com>
2022-12-13 23:25:11 -0800

    Update 034_merge_intervals.cc

--- cpp/patterns/034_merge_intervals.cc
+++ cpp/patterns/034_merge_intervals.cc
@@ -6,8 +6,8 @@
 // Output: [[1,6],[8,10],[15,18]]
 // Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
 
-// Time: O(nlogn) -> sorting.
-// Space: O(n) -> output.
+// Time complexity: O(nlogn) -> sorting.
+// Space complexity: O(n) -> output.
 class Solution {
 public:
     std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
@@ -17,10 +17,15 @@ public:
         std::sort(intervals.begin(), intervals.end());
         std::vector<std::vector<int>> output;
         output.push_back(intervals[0]);
-        
+
+        int last_end, start, end;
         for (int i = 1; i < intervals.size(); i++) {
-            if (output.back()[1] >= intervals[i][0]) {
-                 output.back()[1] = max(output.back()[1] , intervals[i][1]);
+            last_end = output.back()[1];
+            start = intervals[i][0];
+            end = intervals[i][1];
+            if (output.back()[1] >= start) {
+                // Merge trick.
+                output.back()[1] = std::max(last_end, end);
             }
             else {
                 output.push_back(intervals[i]);
@@ -30,8 +35,8 @@ public:
     }
 };
 
-// Time: O(n) -> O(n + n).
-// Space: O(n) -> output and map.
+// Time complexity: O(n) -> O(n + n).
+// Space complexity: O(n) -> output and map.
 class Solution {
 public:
     std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
