efb4d32e0a33844ad4e9a6246ce63570ac80a054
Alex Dubinchyk <hi.alexd@gmail.com>
2022-12-14 14:03:20 -0800

    Update 079_word_search.cc

--- cpp/patterns/backtracking/079_word_search.cc
+++ cpp/patterns/backtracking/079_word_search.cc
@@ -1,89 +1,43 @@
 // Leetcode 79 https://leetcode.com/problems/word-search/
-// Time: O(4n) => O(n).
-// Space: O(1).
-class Solution {
-public:
-   bool exist(std::vector<std::vector<char>> &board, std::string word) {
-       int row = board.size(), col = board[0].size();
-       for (int i = 0 ; i < row ; ++i) {
-           for (int j = 0 ; j < col ; ++j) {
-               if (search(0 , i , j , board , word)) {
-                   return true;
-               }
-           }
-       }
-       return false;
-   }
-
-   bool search(int index , int x , int y , std::vector<std::vector<char>> &board , std::string &word)
-   {
-       if (index == word.size()) {
-           return true;
-       }
-
-       if (x < 0 || y < 0 || x >= board.size() || y >= board[0].size() || board[x][y] == '-') {
-           return false;
-       }
-
-       if (board[x][y] != word[index]) {
-           return false;
-       }
-
-       char temp = board[x][y];
-       board[x][y] = '-'; 
-
-       if (search(index + 1 , x - 1 , y , board , word) || search(index + 1 , x + 1 , y , board , word) || search(index + 1 , x , y - 1 , board , word) || search(index + 1 , x , y + 1 , board , word)) {
-           return true;
-       }
-
-       board[x][y] = temp;
-       return false;
-   }
-};
-
-// DFS.
-// Time: O(N+M) => (O(E+V)).
+// Time: O(4 * (|E| + |V|) => O(|E| + |V|).
 // Space: O(1).
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size(), n = board[0].size();
-        int index = 0;
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if (board[i][j] == word[index]) {
-                    if (search(i, j, index, board, word)) {
-                        return true;
-                    }
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (dfs(board, i, j, 0, word)) {
+                    return true;
                 }
             }
         }
 
         return false;
     }
-    
-    bool search(int i, int j, int index, vector<vector<char>>& board, string& word) {
-        int m = board.size(), n = board[0].size();
-        
+
+    bool dfs(vector<vector<char>>& board, int i, int j, int index, string word) {
         if (index == word.size()) {
-            return true;
-        }
-        if (i < 0 or j < 0 or i >= m or j >= n) {
-            return false;
+           return true;
+       }
+        if (i < 0 || j < 0
+           || i >= board.size() || j >= board[0].size()
+           || board[i][j] != word[index]
+           ) {
+           return false;
         }
 
-        bool result = false;
-        if (word[index] == board[i][j]) {
-            char temp = word[index];
-            board[i][j] = '#';
-            result = search(i+1, j, index+1, board, word) || search(i-1, j, index+1, board, word) || 
-                search(i, j+1, index+1, board, word) || search(i, j-1, index+1, board, word);
-            
-            board[i][j] = temp;
+        board[i][j] = '#';
+        int new_index = index + 1;
+        if (
+            dfs(board, i-1, j, new_index, word) || // up
+            dfs(board, i+1, j, new_index, word) || //down
+            dfs(board, i, j-1, new_index, word) || // left
+            dfs(board, i, j+1, new_index, word) // right
+        ) {
+            return true;
         }
-        
-        return result;
+        board[i][j] = word[index];
+
+        return false;
     }
-    
 };
