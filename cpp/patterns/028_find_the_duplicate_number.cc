93322403249f7362fa460bfedc8ef66359862dda
Alex Dubinchyk <hi.alexd@gmail.com>
2022-12-18 20:47:09 -0800

    Update 028_find_the_duplicate_number.cc

--- cpp/patterns/028_find_the_duplicate_number.cc
+++ cpp/patterns/028_find_the_duplicate_number.cc
@@ -15,18 +15,45 @@ public:
 
     // Fast and Slow, O(n).
     int findDuplicateFastAndSlow(vector<int>& nums) {
-		int slow = nums[0];
-		int fast = nums[nums[0]];
-		while (slow != fast) {
-			slow = nums[slow];
-			fast = nums[nums[fast]];
-		}
+	int slow = nums[0];
+	int fast = nums[nums[0]];
+	while (slow != fast) {
+		slow = nums[slow];
+		fast = nums[nums[fast]];
+	}
 
-		fast = 0;
-		while (fast != slow) {
-			fast = nums[fast];
-			slow = nums[slow];
-		}
-		return slow;
+	fast = 0;
+	while (fast != slow) {
+		fast = nums[fast];
+		slow = nums[slow];
+	}
+	return slow;
+    }
+};
+
+// BST magic(Onlogn)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size(), cnt;
+
+        while (low <=  high) {
+            int mid = low + (high - low) / 2;
+            cnt = 0;
+            // cnt number less than equal to mid
+            for (int n : nums) {
+                if (n <= mid)
+                    cnt++;
+            }
+            // binary search on left
+            if (cnt <= mid) {
+                low = mid + 1;
+            }
+            else {
+                // binary search on right
+                high = mid - 1;
+            }
+        }
+        return low;
     }
 };
